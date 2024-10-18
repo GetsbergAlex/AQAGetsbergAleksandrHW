b55f83e137b5c197728f5a207d40b41a5a41e169
GetsbergAlex <aleksander060585@gmail.com>
2024-10-18 19:24:14 +0300

    homework2

--- src/Homework2/Main.java
+++ src/Homework2/Main.java
@@ -1,8 +1,11 @@
 package Homework2;
 
-public class Employee {
+public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee("Попов", "Менеджер", "alex2.mail.ru", 357568, 4500, 45);
+        Park park = new Park("Диво-остров");
+        Park.Attraction attraction = park.new Attraction(100, "10:00-19:00", "Ракета");
+        Park.Attraction attraction2 = park.new Attraction(135, "10:00-19:00", "Карусель");
         Employee[] persArray = new Employee[5];
         persArray[0] = new Employee("Ivanov Ivan", "Engineer", "Ivanov1@mail.ru", 459768, 40000, 28);
         persArray[1] = new Employee("Popov Andrey", "Senior Manager", "Popov@mail.ru", 537869, 70000, 38);
@@ -10,31 +13,9 @@ public class Employee {
         persArray[3] = new Employee("Petrov Dmitry", "Middle Manager", "Petrov@mail.ru", 846399, 40000, 26);
         persArray[4] = new Employee("Andreev Aleksei", "Junior Manager", "Andreev@mail.ru", 874010, 30000, 22);
         employee.info();
-    }
-
-    private String name;
-    private String position;
-    private String email;
-    private int phone;
-    private int salary;
-    private int age;
-
-    public Employee(String name, String position, String email, int phone, int salary, int age) {
-        this.name = name;
-        this.position = position;
-        this.email = email;
-        this.phone = phone;
-        this.salary = salary;
-        this.age = age;
-    }
-
-
-    public void info() {
-        System.out.println("Имя:" + name + "; Должность:" + position +
-                "; Почтовый адрес:" + email + "; Телефон:" + phone + "; Зарплата:" + salary + "; Возраст:" + age);
+        attraction.printInfo();
+        attraction2.printInfo();
 
     }
-
 
 }
-
--- src/Homework2/Park.java
+++ src/Homework2/Park.java
@@ -3,12 +3,12 @@ package Homework2;
 public class Park {
     public static void main(String args[]) {
         Park park = new Park("Диво-остров");
-        Park.Attraction attraction =   new Park("Диво-остров").new Attraction(100, "10:00-19:00", "Ракета");
-        Park.Attraction attraction2=   new Park("Диво-остров").new Attraction(135,"10:00-19:00","Карусель");
+        Park.Attraction attraction = park.new Attraction(100, "10:00-19:00", "Ракета");
+        Park.Attraction attraction2 = park.new Attraction(135, "10:00-19:00", "Карусель");
         attraction.printInfo();
         attraction2.printInfo();
-        //park.printInfo();
     }
+
     private String name;
 
     public Park(String name) {
@@ -27,13 +27,14 @@ public class Park {
         }
 
 
-
-        public   void printInfo() {
-            //System.out.println("Название парка:"+ Park.this.name);
-            System.out.println("Название Аттракциона:" + name+"; Стоимость:" + cost + "; Время работы:" + workTime );
+        public void printInfo() {
+            System.out.println("Название парка:" + Park.this.name);
+            System.out.println("Название Аттракциона:" + name + "; Стоимость:" + cost + "; Время работы:" + workTime);
 
         }
 
     }
 
 }
+
+
